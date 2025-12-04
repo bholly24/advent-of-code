b8f3f9e39a35a434b000faddb5d8159a9c33e563
Brendan Holly <bholly24@gmail.com>
2025-12-04 09:02:02 -0500

    Rewrite using DFS to bring solve time for part b from 2000ms to 20

--- src/main/kotlin/aoc2025/day04/DayFour.kt
+++ src/main/kotlin/aoc2025/day04/DayFour.kt
@@ -5,6 +5,7 @@ import utils.MutableGrid
 import java.io.File
 
 class DayFour(filePath: String) {
+    private var totalRemoved = 0
     val tpGrid = MutableGrid<Char>(File(filePath).readLines().map { it.toMutableList() }.toMutableList())
     fun partA(): Int {
         var total = 0
@@ -19,21 +20,25 @@ class DayFour(filePath: String) {
         return total
     }
 
-    fun partB() = bruteForceTpRemoval(tpGrid, 0)
-
-    fun bruteForceTpRemoval(grid: MutableGrid<Char>, totalRemoved: Int): Int {
-        for (y in 0 until grid.yMax) {
-            for (x in 0 until grid.xMax) {
+    fun partB(): Int {
+        for (y in 0 until tpGrid.yMax) {
+            for (x in 0 until tpGrid.xMax) {
                 val co = Coord(x, y)
-                if (grid.get(co) != '@') continue
-                val totalTp = grid.getNeighborsWithDiagonal(co).count { tpGrid.get(it) == '@' }
-                if (totalTp < 4) {
-                    val newGrid = grid.copy()
-                    newGrid.set(Coord(x, y), '.')
-                    return bruteForceTpRemoval(newGrid, totalRemoved + 1)
-                }
+                if (tpGrid.get(co) != '@') continue
+                getNeighborsAndRemove(co)
             }
         }
         return totalRemoved
+    }
+
+    private fun getNeighborsAndRemove(co: Coord) {
+        if (tpGrid.get(co) != '@') return
+        val neighbors = tpGrid.getNeighborsWithDiagonal(co)
+            .filter { tpGrid.get(it) == '@' }
+        if (neighbors.size < 4) {
+            tpGrid.set(Coord(co.x, co.y), '.')
+            totalRemoved++
+            neighbors.forEach { n -> getNeighborsAndRemove(n) }
+        }
     }
 }
